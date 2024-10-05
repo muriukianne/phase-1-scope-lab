0c98426722522bb83e2b5bf2298a7dfa5cec91e6
Anne Muriuki <muriukianne05@gmail.com>
2024-10-05 12:53:37 +0300

    add scope

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+}
+
+console.log(upperCaseCustomerName)
+
+function setBestCustomer(){
+   window.bestCustomer = 'not bob'
+}
+console.log (setBestCustomer)
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'alice'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'grace'
+}
